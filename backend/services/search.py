d52fdbdbf0faf42f0ea22c56ba7ef51650993da9
St Michael Egenamba <michaelegenamba@gmail.com>
2026-08-20 16:57:35 -0700

    Changed search.py from function to class for scalablity and changed book.py to align with it

--- backend/services/search.py
+++ backend/services/search.py
@@ -2,17 +2,17 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from models import Book, Author, Tag
 
-def search_books(db: Session, book: str = None, author: str = None, tag: str = None):
-    query = select(Book).join(Book.authors).join(Book.tags).distinct()
-    if book is not None:
-        query = query.where(Book.title.ilike(f"%{book}%"))
-    if author is not None:
-        query = query.where(Author.name.ilike(f"%{author}%"))
-    if tag is not None:
-        query = query.where(Tag.name == tag)
+class SearchService:
+    def __init__(self, db:Session):
+        self.db = db
 
-    return query
-    
+    def search_books(self, book: str = None, author: str = None, tag: str = None):
+        query = select(Book).join(Book.authors).join(Book.tags).distinct()
+        if book is not None:
+            query = query.where(Book.title.ilike(f"%{book}%"))
+        if author is not None:
+            query = query.where(Author.name.ilike(f"%{author}%"))
+        if tag is not None:
+            query = query.where(Tag.name == tag)
 
-
-
+        return query
